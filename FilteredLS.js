dc1a7d11648ee9ccb52db79130c402b2e0bc8e4f
Niraj Pant <nirajpant7@gmail.com>
2014-12-21 13:43:32 -0600

    finish filtered ls

--- FilteredLS.js
+++ FilteredLS.js
@@ -1,1 +1,12 @@
 var fs = require('fs');
+var path = require('path');
+
+fs.readdir(process.argv[2], function callback(err, list) {
+	var fileExtension = "." + process.argv[3];
+
+	list.forEach(function(entry) {
+		if(path.extname(entry) == fileExtension) {
+			console.log(entry);
+		}
+	});	
+});
