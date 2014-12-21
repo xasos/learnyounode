fde113216be467be67afeabe661c178409e5bffe
Niraj Pant <nirajpant7@gmail.com>
2014-12-21 02:31:27 -0600

    fix sync io

--- MyFirstIO.js
+++ MyFirstIO.js
@@ -1,1 +1,19 @@
 var fs = require('fs');
+
+// Read file synchronously
+var file = fs.readFileSync(process.argv[2]).toString();
+var fileString = file.split("\n");
+console.log(fileString.length);
+
+// Find number of new lines in string
+// var delimiter = "\n";
+// var newLines = 0;
+// var num = fileString.charAt(0)
+// console.log(num);
+// for (var i = 0; i < 50; i++) {
+// 	if (fileString.charAt(i) === delimiter) {
+// 		newLines++;
+// 	}
+// }
+
+// // console.log(newLines);
