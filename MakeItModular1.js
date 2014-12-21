52f2c4b47aef914919353ccad3429458291cc3f3
Niraj Pant <nirajpant7@gmail.com>
2014-12-21 15:04:30 -0600

    add working solution

--- MakeItModular1.js
+++ MakeItModular1.js
@@ -8,20 +8,12 @@ module.exports = function(pathName, extension, callback) {
 		}
 		else {
 			var results = [];
-			// for (var i = 0; i < list.length; i++) {				
-			// 	console.log(path.extname(list[i]));
-			// 	console.log(extension)
-			// 	if(path.extname(list[i]) == extension) {
-			// 		results.push(entry);
-			// 		console.log(results);
-			// 	}
-			// }
-
 			list.forEach(function(entry) {
 				if(path.extname(entry) == extension) {
 					results.push(entry);
 				}
 			});
+			
 			callback(null, results);
 		}
 	});
