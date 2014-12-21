e6f51313fcd1ee10bc263e15289c112cd82c06ab
Niraj Pant <nirajpant7@gmail.com>
2014-12-21 02:35:19 -0600

    MyFirstIO done

--- MyFirstIO.js
+++ MyFirstIO.js
@@ -1,19 +1,5 @@
 var fs = require('fs');
 
-// Read file synchronously
-var file = fs.readFileSync(process.argv[2]).toString();
-var fileString = file.split("\n");
-console.log(fileString.length);
-
-// Find number of new lines in string
-// var delimiter = "\n";
-// var newLines = 0;
-// var num = fileString.charAt(0)
-// console.log(num);
-// for (var i = 0; i < 50; i++) {
-// 	if (fileString.charAt(i) === delimiter) {
-// 		newLines++;
-// 	}
-// }
-
-// // console.log(newLines);
+// Read file synchronously and process into string array
+var file = fs.readFileSync(process.argv[2], 'utf8').split("\n");
+console.log(file.length-1);
