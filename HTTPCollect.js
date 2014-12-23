8c27ea15a20221e32b17b5750ac034daff8344ce
Niraj Pant <nirajpant7@gmail.com>
2014-12-22 21:03:05 -0600

    add working http collect

---
+++ HTTPCollect.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var bl = require('bl');
+
+http.get(process.argv[2], function(response) {
+	var str = "";
+	response.setEncoding("utf8");
+	response.pipe(bl(function (err, data) {
+		console.log(data.toString().length);
+		console.log(data.toString());
+	}));	
+});
